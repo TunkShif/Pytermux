203d3acef260db82cc61e880e50ffbda821b12a5
TunkShif <tunkshif@foxmail.com>
2018-07-20 18:39:00 +0800

    WIP

--- termux/__init__.py
+++ termux/__init__.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import os
-import json
+import os as _os
+import json as _json
 
 __version__ = "0.1"
 
 
 def _run(commands, is_json=False):
-    """Run a shell command and return output
+    """Run Commands
+
+    Run a shell command and return output
 
     Args:
         commands: shell command
         is_json: whether the output is json string
-
     Returns:
         the output or a dict
     """
-    cmd = os.popen(commands)
-    if json:
-        return json.loads(cmd.read())
+    cmd = _os.popen(commands)
+    if is_json:
+        return _json.loads(cmd.read())
     else:
         return cmd.read()
--- termux/dialog.py
+++ termux/dialog.py
@@ -2,9 +2,29 @@
 # -*- coding:utf-8 -*-
 from termux import _run
 
-cmd = "termux-dialog"
+_cmd = "termux-dialog"
 
 
 def confirm(hint="Hint", title="Title"):
-    data = _run(f"{cmd} -i {hint} -t {title}", is_json=True)
+    """Confirm Dialog
+    Returns:
+        dict
+    """
+    data = _run(f"{_cmd} -i {hint} -t {title}", is_json=True)
     return data
+
+
+def checkbox(values: list, title="Title"):
+    """Checkbox Dialog
+    Args:
+        values: a string list
+    Returns:
+        dict
+    """
+    v = ", ".join(values)
+    data = _run(f"{_cmd} -v {v} -t {title}", is_json=True)
+    return data
+
+
+def counter():
+    pass
